0d499e6c94e2530d08406783d4a01e28b17b9f89
ZsoltFischer <zsolt@fisu.hu>
2025-09-03 23:00:09 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/manuflow/stopwatch/stopwatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.manuflow.stopwatch.stopwatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:stopwatch/app/app.dart';
+import 'package:stopwatch/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
